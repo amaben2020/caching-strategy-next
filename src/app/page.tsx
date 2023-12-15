3c3ed0b59881bb0d29578c5fbbd4228e439bc1c5
algomachine007 <amaben202phantom@gmail.com>
2023-12-15 10:41:19 +0100

    checking for latency and data type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,11 @@ export default async function Home() {
 
   const response = await data.json();
 
-  console.log();
+  console.log(response);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>{response.data.type} </h1>
+      <h1>{response.data.latency} </h1>
       {response.data.data.map((data) => (
         <>{data.title}</>
       ))}
